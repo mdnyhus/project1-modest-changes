aa5f62d46c8c9f8d658594d5443a5a0364e6bcf5
mdnyhus <mdnyhus@gmail.com>
2018-02-05 16:36:26 -0800

    added stubs for blockartlib.go canvas functions

---
+++ blockartlib/rpcCommunication.go
@@ -0,0 +1,9 @@
+/*
+
+This file is part of the blockartlib package, and contains all the structs required for RPC calls between blockartlib and an
+ink-miner. It is placed in this file for cleaner organization of code.
+
+*/
+
+package blockartlib
+
