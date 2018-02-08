1f1462d28272a7972a2e2c90e0e50ca5f74ffbad
Wesley Hong <hong.wesley609@gmail.com>
2018-02-08 14:17:32 -0800

    changed

--- blockartlib/test/blockartTest.go
+++ blockartlib/test/blockartTest.go
@@ -7,5 +7,8 @@ func main() {
 	fmt.Println("starting tests...")
 
 	//blockartlib.SvgToShape("<path d=\"M10 10 H 90 V 90 H 10 Z\" fill=\"transparent\" stroke=\"black\"/>")
-	blockartlib.SvgToShape("  <path id=\"lineBC\" d=\"M 250 50 l 150 300\" stroke=\"red\" stroke-width=\"3\" fill=\"none\"/>")
+	shape , err := blockartlib.SvgToShape("  <path id=\"lineBC\" d=\"M 250 50 l 150 300\" stroke=\"red\" stroke-width=\"3\" fill=\"none\"/>")
+	fmt.Println("Created Shape")
+	fmt.Println(shape)
+	panic(err)
 }
