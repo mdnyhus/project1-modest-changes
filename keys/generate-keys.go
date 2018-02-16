05594b3d0d02d72a2aa6d5e6eeae62b63455a008
Justin Chau <justin.chau623@gmail.com>
2018-02-15 20:43:17 -0800

    Checking in keys

---
+++ keys/generate-keys.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	for i := 0; i < 20; i++ {
+		keyPointer, _ := ecdsa.GenerateKey(elliptic.P224(), rand.Reader)
+		privKey := *keyPointer
+		pubByte, _ := x509.MarshalPKIXPublicKey(&privKey.PublicKey)
+		privByte, _ := x509.MarshalECPrivateKey(&privKey)
+		pubStr := hex.EncodeToString(pubByte)
+		privStr := hex.EncodeToString(privByte)
+		fmt.Println("=======" + strconv.Itoa(i+1) + "=======")
+		fmt.Println("PUBLIC: " + pubStr)
+		fmt.Println("PRIVATE: " + privStr)
+	}
+}
